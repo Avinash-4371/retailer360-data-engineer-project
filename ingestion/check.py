9334e2571258c643ac03918839a31d712b41149f
Avinash <saibittu4371@gmail.com>
2026-05-24 22:56:43 +0530

    Testing valid_file_extension method

--- ingestion/check.py
+++ ingestion/check.py
@@ -14,12 +14,13 @@ def validate_file_extension(file_path : Path, file_type: str):
                  }
     recive = extension_required.get(file_type)
     for files in Path(file_path).iterdir():
-        if files.suffix.lower() not in recive:
+        if files.is_file() and files.suffix.lower() not in recive:
 
-           raise ValueError(f"invalid extension {Path(file_path).suffix} for file type {file_type}")
+           raise ValueError(f"Invalid extension {files.suffix} for file type {file_type} in file {files.name}")
+    return True
 
 
-a=list_matching_files("C:/Users/avi72/retailer360-data-engineer-project/data/landing/sales/","sales_*.csv")
-print(a)
+#a=list_matching_files("C:/Users/avi72/retailer360-data-engineer-project/data/landing/sales/","sales_*.csv")
+#print(a)
 b=validate_file_extension("C:/Users/avi72/retailer360-data-engineer-project/data/landing/sales/","csv")
 print(b)
